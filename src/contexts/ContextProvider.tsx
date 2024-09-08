7ed43d83021dfc821bf598cf513f69f4d6944b29
WardenBradley <nazmuzzaman13@gmail.com>
2024-09-09 02:11:08 +0530

    new changes

--- src/contexts/ContextProvider.tsx
+++ src/contexts/ContextProvider.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
-
-const ContextProvider = () => {
-  return (
-    <div>ContextProvider</div>
-  )
-}
-
-export default ContextProvider
+import { WalletAdapterNetwork, WalletError } from "@solana/wallet-adapter-base";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletModalProvider as ReactUIWalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import {
+  PhantomWalletAdapter,
+  SolflareWalletAdapter,
+  SolletExtensionWalletAdapter,
+  SolletWalletAdapter,
+  TorusWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
