fc2a9037c1debff7a0a5ba41f89eb39e360362fe
WardenBradley <nazmuzzaman13@gmail.com>
2024-09-09 01:56:48 +0530

    primero commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import { AppProps } from "next/app";
+import Head from "next/head";
+import { FC } from "react";
+// @ts-ignore
+import { ContextProvider } from "../contexts/ContextProvider";
+import AppBar from "components/AppBar";
+// @ts-ignore
+import { Footer } from "../components/Footer";
+import Notifications from "../components/Notification";
 
-const _app = () => {
+require("@solana/wallet-adapter-react-ui/styles.css");
+require("../styles/globals.css");
+
+const App: FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <div>_app</div>
-  )
-}
+    <div className="bg-default-900">
+      <Head>
+        <title>Solana Token Creator</title>
+      </Head>
+      <ContextProvider>
+        <Notifications />
+        <AppBar />
+        <Component {...pageProps} />
+        <Footer />
+      </ContextProvider>
+      // SCRIPTS
+      <script src="assets/libs/preline/preline.js"></script>
+      <script src="assets/libs/swiper/swiper-bundle.min.js"></script>
+      <script src="assets/libs/gumshoejs/gumshoe.polyfills.min.js"></script>
+      <script src="assets/libs/lucide/lucide.min.js"></script>
+      <script src="assets/libs/aos/aos.js"></script>
+      <script src="assets/js/swiper.js"></script>
+      <script src="assets/js/theme.js"></script>
+    </div>
+  );
+};
 
-export default _app
+export default App;
