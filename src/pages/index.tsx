426de2ac12f48a79ac67994b6fbeca42f5c8bb12
WardenBradley <nazmuzzaman13@gmail.com>
2024-09-10 21:35:41 +0530

    Tuesday

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,90 @@
-import React from 'react'
+import React, { useState } from "react";
+import type { NextPage } from "next";
+import Head from "next/head";
 
-const index = () => {
+//INTERNAL IMPORT
+import {
+  HomeView,
+  ToolView,
+  FeatureView,
+  OfferView,
+  FaqView,
+  CreateView,
+  TokenMetadata,
+  ContactView,
+  AirdropView,
+  DonateView,
+} from "../views";
+
+const Home: NextPage = (props) => {
+  //STATE VARIABLE
+  const [openCreateModal, setOpenCreateModal] = useState(false);
+  const [openTokenMetaData, setOpenTokenMetaData] = useState(false);
+  const [openContact, setOpenContact] = useState(false);
+  const [openAirdrop, setopenAirdrop] = useState(false);
+  const [openSendTransaction, setOpenSendTransaction] = useState(false);
+
   return (
-    <div>index</div>
-  )
-}
+    <>
+      <Head>
+        <title>Solana Token Creator</title>
+        <meta
+          name="Solana Token Creator"
+          content="Build & Create solana token"
+        />
+      </Head>
 
-export default index
+      <HomeView setOpenCreateModal={setOpenCreateModal} />
+      <ToolView
+        setopenAirdrop={setopenAirdrop}
+        setOpenContact={setOpenContact}
+        setOpenCreateModal={setOpenCreateModal}
+        setOpenSendTransaction={setOpenSendTransaction}
+        setOpenTokenMetaData={setOpenTokenMetaData}
+      />
+      {/*
+      <FeatureView
+        setopenAirdrop={setopenAirdrop}
+        setOpenContact={setOpenContact}
+        setOpenCreateModal={setOpenCreateModal}
+        setOpenSendTransaction={setOpenSendTransaction}
+        setOpenTokenMetaData={setOpenTokenMetaData}
+      />
+
+      <OfferView />
+      <FaqView />
+      //DYNAMIC COMPONENTS
+      {openCreateModal && (
+        <div className="new_loader relative h-full bg-slate-900">
+          <CreateView setOpenCreateModal={setOpenCreateModal} />
+        </div>
+      )}
+
+      {openTokenMetaData && (
+        <div className="new_loader relative h-full bg-slate-900">
+          <TokenMetadata setOpenTokenMetaData={setOpenTokenMetaData} />
+        </div>
+      )}
+
+      {openContact && (
+        <div className="new_loader relative h-full bg-slate-900">
+          <ContactView setOpenContact={setOpenContact} />
+        </div>
+      )}
+
+      {openAirdrop && (
+        <div className="new_loader relative h-full bg-slate-900">
+          <AirdropView setopenAirdrop={setopenAirdrop} />
+        </div>
+      )}
+
+      {openSendTransaction && (
+        <div className="new_loader relative h-full bg-slate-900">
+          <DonateView setOpenSendTransaction={setOpenSendTransaction} />
+        </div>
+      )} */}
+    </>
+  );
+};
+
+export default Home;
---
+++ src/views/index.tsx
@@ -0,0 +1,21 @@
+export { HomeView } from "./home";
+//@ts-ignore
+export { ToolView } from "./tools";
+//@ts-ignore
+export { FeatureView } from "./feature";
+//@ts-ignore
+export { OfferView } from "./offer";
+//@ts-ignore
+export { FaqView } from "./faq";
+//@ts-ignore
+export { AirdropView } from "./airdrop";
+//@ts-ignore
+export { DonateView } from "./donate";
+//@ts-ignore
+export { CreateView } from "./create";
+//@ts-ignore
+export { InputView } from "./input";
+//@ts-ignore
+export { TokenMetadata } from "./tokenMetadata";
+//@ts-ignore
+//export { ContactView } from "./contact";
